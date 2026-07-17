47241145f91895fd3ebeaea1004ae566227e2745
watson limprevil <limprevil35@gmail.com>
2026-07-17 02:32:10 -0400

    changed the summary section of the app

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -3,22 +3,24 @@ export default function Summary({summary}){
 
   return(
    
-    <div className='section' >
-       <h2>Summary</h2>
-       <p><strong>Total Income</strong>${summary.income}</p>
-       <p><strong>Total expenses</strong>${summary.expenses}</p>
-       <p><strong>Net:</strong>${summary.net}</p>
+   <div className="highlight-bar">
 
+  <div className="highlight-item income">
+    <span className="label">Income</span>
+    <span className="value">${summary?.totalIncome || 0}</span>
+  </div>
 
-       <h3>This Month</h3>
-       <p>income: ${summary.thisMonth.income}</p>
-       <p>Expenses: ${summary.thisMonth.expenses}</p>
-       <p>Net: ${summary.thisMonth?.net}</p>
+  <div className="highlight-item expenses">
+    <span className="label">Expenses</span>
+    <span className="value">${summary?.totalExpenses || 0}</span>
+  </div>
 
-       <h3>Last 30 Days</h3>
-       <p>Income: ${summary.last30Days?.income}</p>
-       <p>Expenses:${summary.last30Days?.expenses}</p>
-       <p>Net: ${summary.last30Days?.net}</p>
-    </div>
+  <div className="highlight-item net">
+    <span className="label">Net</span>
+    <span className="value">${summary?.net || 0}</span>
+  </div>
+
+</div>
+
   )
 }
